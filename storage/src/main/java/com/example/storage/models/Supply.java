96db570ba0df5788ead5eee099783ede025f1fb0
unknown <99maxwolf99@gmail.com>
2024-03-28 21:16:41 +0300

    add eureka-server

---
+++ storage/src/main/java/com/example/storage/models/Supply.java
@@ -0,0 +1,20 @@
+package com.example.storage.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "supplies")
+public class Supply {
+    @Id
+    private Long id;
+}
